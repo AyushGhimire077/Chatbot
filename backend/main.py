f542343f45f6d0dd6cc3bdb3102f75f403e1a3ed
Aayush Ghimire <ayushghimire077@gmail.com>
2025-03-25 13:48:18 +0545

    Defined port in main.py

--- backend/main.py
+++ backend/main.py
@@ -103,3 +103,9 @@ async def chatbot(request_data: ChatModel):
 
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail=f"Request failed: {str(e)}")
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
